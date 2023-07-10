431cfd0d399ab4704ad14121152dc4805e06c6a9
jatinsharma2717 <jatin.sharma.aj@gmail.com>
2023-07-11 01:41:01 +0530

    mongosse updated

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,9 @@
-import mongoose from 'mongoose';
-
-const uri: string = 'mongodb+srv://jatinsharmaaj:123456Jhu@cluster0.nxypk97.mongodb.net/MLM';
-
-export const connectDatabase = async () => {
-  try {
-    mongoose.connect(uri, {
-    });
-
-    console.log('Connected to the database');
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-    process.exit(1);
-  }
-};
+import * as mongoose from "mongoose";
+const uri: string = "mongodb+srv://jatinsharmaaj:123456Jhu@cluster0.nxypk97.mongodb.net/MLM";
+mongoose.connect(uri, (err: any) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log("Successfully Connected!");
+    }
+});
--- src/server.ts
+++ src/server.ts
@@ -5,39 +5,21 @@ import * as database from "./database";
 import usersRouter from "./routes/user.route";
 import paymentDetailsRouter from "./routes/payment.route";
 import adminRouter from "./routes/admin.route";
-import { connectDatabase } from "./database";
-
-// Our Express APP config
-
-// API Endpoints
-
-const startServer = async () => {
-  // Connect to the database
-  await connectDatabase();
-
-  // Rest of your application code here
-};
-
+const databases = database
 // Our Express APP config
 const app = express();
 app.use(express.json());
 
 app.use(cors());
 app.set("port", process.env.PORT || 3000);
-const http = require("http").createServer(app);
-startServer()
-
-http.listen(5000, () => {
-  console.log("listening on *:5000");
-});
 // API Endpoints
-app.get("/", (req: any, res: any) => {
-  res.send("Hello World!");
-});
+app.get('/', (req:any, res:any) => {
+  res.send('Hello World!')
+})
 app.use("/api/v1/user", usersRouter);
 app.use("/api/v1/user", paymentDetailsRouter);
 app.use("/api/v1/admin", adminRouter);
 
-export const server = app.listen(app.get("port"), () => {
+const server = app.listen(app.get("port"), () => {
   console.log("App is running on http://localhost:%d", app.get("port"));
 });
