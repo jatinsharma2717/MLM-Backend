c6deb6488b6f11e630790444d527cd45dd10ef55
jatinsharma2717 <jatin.sharma.aj@gmail.com>
2023-07-10 22:42:09 +0530

    id generate

--- src/services/userid.service.ts
+++ src/services/userid.service.ts
@@ -4,11 +4,15 @@ import { Users } from "../modals/user";
 export const generateUserId = async () => {
   try {
     // Find the highest user ID in the database
-    const highestIdUser = await Users.findOne().sort({ userid: -1 });
+    const highestIdUser = await Users.findOne().sort({ userid: -1 }).lean();
+
+    // Extract the highest ID and handle the case when no user is found
     let highestId = highestIdUser ? highestIdUser.userid : 0;
 
     // Increment the highest ID by 1 for the new user
-    return highestId + 1;
+    const newUserId = highestId + 1;
+
+    return newUserId;
   } catch (error) {
     // Handle any errors that occurred during the process
     console.error("Error generating user ID:", error);
