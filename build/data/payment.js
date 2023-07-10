c99d30259498867601120111027689e141260641
jatinsharma2717 <jatin.sharma.aj@gmail.com>
2023-07-10 22:16:10 +0530

    nodemon updated ts node serverts

--- build/data/payment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.paymentDetailsResponse = void 0;
-exports.paymentDetailsResponse = {
-    message: "Payment Details successfully",
-    statuscode: 200,
-    error: false,
-};
-//# sourceMappingURL=payment.js.map
